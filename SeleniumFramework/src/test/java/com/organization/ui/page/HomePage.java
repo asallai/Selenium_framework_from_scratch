f58a4edd0c179b10a3d56fae473e468d64772d74
Aniko Sallai <aniko.sallai@gmail.com>
2023-07-19 20:44:33 +0200

    pom updated

--- SeleniumFramework/src/test/java/com/organization/ui/page/HomePage.java
+++ SeleniumFramework/src/test/java/com/organization/ui/page/HomePage.java
@@ -13,6 +13,16 @@ public class HomePage {
     }
 
     private static final By AGREE_BUTTON = By.xpath("//button[contains(text(), 'Yes, I agree')]");
+    /*
+      xpath
+        //input[@id="input-button__departure"]/ancestor::div[2]/fsw-input-button//input/ancestor::fsw-input-button
+      css
+        #input-button__departure
+        .input-button__input.ng-star-inserted
+        html > head > base
+        html > body span._container.icon-ie-160-32
+        html > body input[placeholder='Departure']
+     */
 
     public void clickAgreeButton() {
         WebElement element = driver.findElement(AGREE_BUTTON);
