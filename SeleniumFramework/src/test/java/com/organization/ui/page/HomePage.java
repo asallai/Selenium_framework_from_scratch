4f979b12f8e2e8a54f0a8df4bb477488f3d4f3e1
Aniko Sallai <aniko.sallai@gmail.com>
2023-07-22 21:53:59 +0200

    Selectors defined

--- SeleniumFramework/src/test/java/com/organization/ui/page/HomePage.java
+++ SeleniumFramework/src/test/java/com/organization/ui/page/HomePage.java
@@ -12,9 +12,9 @@ public class HomePage {
         this.driver = webDriver;
     }
 
-    private static final By AGREE_BUTTON = By.xpath("//button[contains(text(), 'Yes, I agree')]");
-    /*
-      xpath
+     /*
+      Some examples:
+       xpath
         //input[@id="input-button__departure"]/ancestor::div[2]/fsw-input-button//input/ancestor::fsw-input-button
       css
         #input-button__departure
@@ -23,6 +23,20 @@ public class HomePage {
         html > body span._container.icon-ie-160-32
         html > body input[placeholder='Departure']
      */
+
+    /*
+        Departure field:
+           //input[@id='input-button__departure']
+           #input-button__departure
+        Destination field:
+           //input[@id='input-button__destination']
+           #input-button__destination
+        Search button:
+           //button[@data-ref='flight-search-widget__cta']
+           .flight-search-widget__start-search.ng-tns-c54-3.ry-button--gradient-yellow
+     */
+
+    private static final By AGREE_BUTTON = By.xpath("//button[contains(text(), 'Yes, I agree')]");
 
     public void clickAgreeButton() {
         WebElement element = driver.findElement(AGREE_BUTTON);
